319c9bc53f145f36069f4ab4b5e7ffde3e48da19
BerdinaKatya <kuznkate42@gmail.com>
2023-07-21 20:39:55 +0500

    Setup app logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redTrafficLightView: UIView!
+    @IBOutlet var yellowTrafficLightView: UIView!
+    @IBOutlet var greenTrafficLightView: UIView!
+    @IBOutlet var trafficLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redTrafficLightView.layer.cornerRadius = 50
+        yellowTrafficLightView.layer.cornerRadius = 50
+        greenTrafficLightView.layer.cornerRadius = 50
+        trafficLightButton.layer.cornerRadius = 20
     }
 
 
+    @IBAction func switchTrafficLightButtonTapped() {
+        trafficLightButton.setTitle("NEXT", for: .normal)
+        
+        if redTrafficLightView.alpha == yellowTrafficLightView.alpha {
+            redTrafficLightView.alpha = 1
+            greenTrafficLightView.alpha = 0.3
+        } else if redTrafficLightView.alpha > 0.3
+                    && yellowTrafficLightView.alpha == greenTrafficLightView.alpha {
+            yellowTrafficLightView.alpha = 1
+            redTrafficLightView.alpha = 0.3
+        } else if yellowTrafficLightView.alpha > 0.3
+                    && redTrafficLightView.alpha == greenTrafficLightView.alpha {
+            greenTrafficLightView.alpha = 1
+            yellowTrafficLightView.alpha = 0.3
+        }
+    }
 }
 
